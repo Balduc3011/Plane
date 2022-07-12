850005fd4cbd1bb9f9a5369d4a0ea71dfd48952a
Balduc3011 <ducnb3011@gmail.com>
2022-07-12 11:59:58 +0700

    Second Commit

--- Assets/Script/PlaneControl.cs
+++ Assets/Script/PlaneControl.cs
@@ -8,9 +8,11 @@ public class PlaneControl : MonoBehaviour
     public float flySpeed = 5f;
     public float yawSpeed = 120;
 
-    public GameObject partPrefab;
-    public List<GameObject> bodyParts = new List<GameObject>();
-    public List<Vector3> pastPosition = new List<Vector3>();
+    public float BodySpeed = 5;
+    public int Gap = 10;
+    public GameObject BodyPrefab;
+    private List<GameObject> BodyParts = new List<GameObject>();
+    private List<Vector3> PositionsHistory = new List<Vector3>();
     int gap = 10;
     public float bodySpeed = 5;
 
@@ -26,7 +28,7 @@ public class PlaneControl : MonoBehaviour
     void Update()
     {
         pointer.transform.position = transform.position;
-        transform.position +=  transform.forward * flySpeed * Time.deltaTime;
+        transform.position += transform.forward * flySpeed * Time.deltaTime;
 
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
@@ -37,21 +39,29 @@ public class PlaneControl : MonoBehaviour
 
         transform.localRotation = Quaternion.Euler(Vector3.up * yaw + Vector3.right * pitch + Vector3.forward * roll);
 
-        pastPosition.Insert(0, transform.position);
+        // Store position history
+        PositionsHistory.Insert(0, transform.position);
+
+        // Move body parts
         int index = 0;
-        foreach (var body in bodyParts)
+        foreach (var body in BodyParts)
         {
-            Vector3 point = pastPosition[Mathf.Min(index * gap, pastPosition.Count - 1)];
+            Vector3 point = PositionsHistory[Mathf.Clamp(index * Gap, 0, PositionsHistory.Count - 1)];
+
+            // Move body towards the point along the snakes path
             Vector3 moveDirection = point - body.transform.position;
-            body.transform.position += moveDirection * bodySpeed * Time.deltaTime;
+            body.transform.position += moveDirection * BodySpeed * Time.deltaTime;
+
+            // Rotate body towards the point along the snakes path
             body.transform.LookAt(point);
+
             index++;
         }
     }
 
     void GrowPlane()
     {
-        GameObject part = Instantiate(partPrefab);
-        bodyParts.Add(part);
+        GameObject body = Instantiate(BodyPrefab);
+        BodyParts.Add(body);
     }
 }
